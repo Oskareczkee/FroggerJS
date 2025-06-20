4469d411c4a8553c7740f0cc6406519125b1630e
Oskareczkee <osniedz@gmail.com>
2025-06-20 22:25:35 +0200

    Add captcha production

--- php/config/config.php
+++ php/config/config.php
@@ -1,11 +1,16 @@
 <?php 
     define('PROJECT_ROOT', __DIR__.'../..');
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_USER', 'admin');
+    define('DB_PASS', 'sluchajszczyluzafajdany');
     define('DB_CHARSET', 'utf8mb4');
     define('DB_NAME', 'froggerdb');
 
-    define('CAPTCHA_SECRET', '6Le502IrAAAAAMxqjWe-cAMTFvpoPwn6ycP3aZfM');
-    define('CAPTCHA_PUBLIC', '6Le502IrAAAAAFnUrD5A4ZvloKE3W5Z4GotA1idz');
+    #local
+    #define('CAPTCHA_SECRET', '6Le502IrAAAAAMxqjWe-cAMTFvpoPwn6ycP3aZfM');
+    #define('CAPTCHA_PUBLIC', '6Le502IrAAAAAFnUrD5A4ZvloKE3W5Z4GotA1idz');
+
+    #prod
+    define('CAPTCHA_SECRET', '6Lea_mcrAAAAAFn8pL6Xf9M72zYbL39S6egBDHAS');
+    define('CAPTCHA_PUBLIC', '6Lea_mcrAAAAALuOYn5fpu08DU0MkxXGqScURNR0');
 ?>
